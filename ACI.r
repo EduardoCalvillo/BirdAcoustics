f6c3b9a2ab00b8126ca37f2a82415429b82b1dc1
unknown <jecalvillo@hotmail.com>
2019-11-24 16:52:59 -0600

    Start Repo

--- ACI.r
+++ ACI.r
@@ -17,13 +17,24 @@ NewACI <- function(wave) {
 }
 p <- "C:/Users/Eduardo Calvillo Uni/Documents/FIME/Topicos Selectos 2/AUDIOS PIA/Partes/"
 list <- unlist(list.files(path = p, pattern = ".*\\.wav", full.names = TRUE))
+
+#Tratamiento
 waves <- lapply(list, readWave)
-monoWaves <- unlist(lapply(waves, mono))
-list
+waves <- unlist(lapply(waves, mono))
+
+myFFilter <- function(wave, from, to, bandpass) {
+    f <- wave@samp.rate
+    wave <- ffilter(wave, f= f, from = from, to = to, bandpass = bandpass)
+    return(wave)
+}
+waves <- unlist(lapply(waves, myFFilter, from = 8000, to = 9200, bandpass = FALSE ))
+
+
 ACIs <- lapply(newlist, NewACI)
 amplitudes <- lapply(monoWaves, env, envt = "abs", plot = FALSE)
 newlist <- monoWaves[1:10]
 ACIs
+waves
 specs <- lapply(newlist, myspec)
 head(specs[[1]]) 
 
