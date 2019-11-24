154663482e42480542015a47a676e67bc0de0654
unknown <jecalvillo@hotmail.com>
2019-11-24 17:31:55 -0600

    Pretty code

--- ACI.r
+++ ACI.r
@@ -1,22 +1,29 @@
 library(seewave)
 library(tuneR)
+library(soundecology)
 
 NewACI <- function(wave) {
     output <- ACI(wave)
-    cat("finished")
+    cat("finished\n")
     return(output)
 }
 
 myFFilter <- function(wave, from, to, bandpass) {
     f <- wave@samp.rate
     wave <- ffilter(wave, f= f, from = from, to = to, bandpass = bandpass, output = "Wave")
-    cat("finished")
+    cat("finished\n")
     return(wave)
 }
 
 myspec <- function(wave) {
-    cat("finished")
+    cat("finished\n")
     return(spec(wave, f = as.numeric(wave@samp.rate), plot = FALSE)[,"y"])
+}
+
+
+myBI <- function(wave) {
+    invisible(capture.output(output <- bioacoustic_index(wave)))
+    return(output$left_area)
 }
 
 #Leer Directorio
@@ -32,7 +39,9 @@ waves <- lapply(waves, myFFilter, from = 8000, to = 9200, bandpass = FALSE )
 # Verificar que el filtro de frecuencia se haya aplicado
 # spec(waves[[2]], f = as.numeric(waves[[2]]@samp.rate), plot = TRUE)
 
-ACIs <- lapply(newlist, NewACI)
+#Calcular BI
+BIs <- lapply(waves, myBI)
+BIs
 
 # Tratamiento de Amplitudes (Transformación logarítimca)
 # amplitudes <- lapply(monoWaves, env, envt = "abs", plot = FALSE)
@@ -42,3 +51,7 @@ logAmplitudes <- lapply(amplitudes, log1p)
 # Tratamiento de Frecuencias (Transformación logarítimca)
 # specs <- lapply(newlist, myspec)
 logFrequencies <- lapply(specs, log1p)
+
+#Calcular ACI
+ACIs <- lapply(waves, NewACI)
+ACIs
