755c3d33a76c26ac23f661f1087f034a27106878
unknown <jecalvillo@hotmail.com>
2019-11-26 01:11:59 -0600

    Added test for cor() function

---
+++ CorTest.r
@@ -0,0 +1,13 @@
+paquete <- 5 #Número de muestras
+nrow = paquete*4 #Hay 4 divisiones (Sin, Bajo, Medio, Alto)
+qty <- 10 #Numero de loops
+for(i in 1:qty){
+    cat("Test", i,"\n")
+    y <- c(rep(0,paquete),rep(1,paquete),rep(2,paquete),rep(3,paquete)) # 0 Sin Presencia, 1 Baja Presencia, 2 Media Presencia, 3 Alta Prescencia
+    x <- c(runif(paquete,0,50),runif(paquete,50,100),runif(paquete,100,150),runif(paquete,150,200)) #Numeros aleatorios pero con limites marcados por nivel de prescencia
+    # matrix(c(y,x), nrow = nrow)
+    z <- runif(nrow,0,200) #Numeros completamente aleatorios 
+    # matrix(c(y,z), nrow= nrow)
+    print(cor(x,y, method = "spearman")) #Esto siempre tiene correlación >.9, lo cual es correcto a lo que deseamos, hay una relación ASCENDENTE
+    print(cor(z,y, method = "spearman")) #Esto da valores muy aleatorios. Muchas veces cercanos a 0, lo cual indica que NO hay correlación
+}
