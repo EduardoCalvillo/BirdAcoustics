9d679e1bd5734d8eb62a0432a286a88d80409ce8
unknown <jecalvillo@hotmail.com>
2019-11-27 00:11:01 -0600

    "Added normalizing feature"

--- ACI.r
+++ ACI.r
@@ -43,67 +43,109 @@ IdGroup <- function(BIS, IBmax, IBmin){
     }
     return(output)
 }
+
+
+logVar <- function(x, var) {
+    # print(paste(x,var+1))
+    return(log(x+var+1))
+}
+
 BIs <- c()
 ACIs <-c()
+BIs.norm <- c()
+ACIs.norm <-c()
+
 sink("logs.txt")
 #Leer Directorio
 p <- "C:/Users/Eduardo Calvillo Uni/Documents/FIME/Topicos Selectos 2/AUDIOS PIA/Partes/"
 list <- unlist(list.files(path = p, pattern = ".*\\.wav", full.names = TRUE))
 list <- list[20:25]
+
 cat("==List of files analized\n")
 print(list)
+
 #Tratamiento
 sink()
 for(i in 1:length(list)){
     cat("Treating wave #",i,"\n")
-    wave <- mono(readWave(list[i]))
-    wave <- myFFilter(wave,from = 8000, to = 9200, bandpass = FALSE )
-    # Verificar que el filtro de frecuencia se haya aplicado
-    # spec(waves[[2]], f = as.numeric(waves[[2]]@samp.rate), plot = TRUE)
+    wave.og <- mono(readWave(list[i]))
+    wave.og <- myFFilter(wave.og,from = 8000, to = 9200, bandpass = FALSE )
+    
+    # Normalizar el audio utilizandolo su envoltura absoluta
+    # wave.abs <- env(wave.og, envt = "abs", plot = FALSE)
+    # wave.abslog.left <- unlist(lapply(wave.abs, logVar, var = 0))
+    # wave.abslog <- Wave(left = wave.abslog.left, samp.rate = as.numeric(wave.og@samp.rate))
+    
+    #Se normaliza el audio 
+    m<- abs(min(wave.og@left))
+    wave.norm.left <- unlist(lapply(wave.og@left, logVar, var = m))
+    wave.norm <- Wave(left = wave.norm.left, samp.rate = as.numeric(wave.og@samp.rate), bit = 16)
+
+    #Visualizar sinusoidales
+    # par(mfrow=c(3,1))
+    # plot(wave.og)
+    # plot(wave.norm)
+    # plot(wave.abslog)
 
     #Calcular BI
-    BIs <- c(BIs,myBI(wave))
-
-    # Tratamiento de Amplitudes (Transformación logarítimca)
-    # amplitudes <- lapply(monoWaves, env, envt = "abs", plot = FALSE)
-    # logAmplitudes <- lapply(amplitudes, log1p)
-    # log1p(amplitudes[[1]])
-
-    # Tratamiento de Frecuencias (Transformación logarítimca)
-    # specs <- lapply(newlist, myspec)
-    # logFrequencies <- lapply(specs, log1p)
-
+    BIs <- c(BIs,myBI(wave.og))
+    BIs.norm <- c(BIs.norm, myBI(wave.norm))
     #Calcular ACI
-    ACIs <- c(ACIs,NewACI(wave))
+    ACIs <- c(ACIs,NewACI(wave.og))
+    ACIs.norm <- c(ACIs.norm,NewACI(wave.norm))
 }
 sink("logs.txt", append = TRUE)
 
 cat("\n==BIs calculated\n")
 print(BIs)
 
+cat("\n==Normalized BIs calculated\n")
+print(BIs.norm)
+
 ##Id-Indices Min-Max
 IBmax <- max(BIs)
 IBmin <- min(BIs)
 
-#Identificamos el grupo al que pertencere
-#Cada uno de los waves
+IBmax.norm <- max(BIs.norm)
+IBmin.norm <- min(BIs.norm)
+
+#Identificamos el grupo al que pertence cada uno de los waves
 Groups <- IdGroup(BIs,IBmax,IBmin)
 Groups <- t(matrix(Groups,nrow=2))
 cat("\n==BIs Groups\n")
 print(Groups)
 
+Groups.norm <- IdGroup(BIs.norm,IBmax.norm,IBmin.norm)
+Groups.norm <- t(matrix(Groups.norm,nrow=2))
+cat("\n==Normalized BIs Groups\n")
+print(Groups)
+
 cat("\n==ACIs calculated\n")
 print(ACIs)
+cat("\n==Normalized ACIs calculated\n")
+print(ACIs.norm)
+
 
 Final <- cbind(Groups, ACIs)
 colnames(Final) <- c("Group","BI","ACI")
 cat("\n==Final Table\n")
 print(Final)
 
+Final.norm <- cbind(Groups.norm, ACIs.norm)
+colnames(Final.norm) <- c("Group","BI","ACI")
+cat("\n==Normalized Final Table\n")
+print(Final.norm)
+
 correlation <- cor(Final[,"Group"], Final[,"ACI"],method = "spearman")
 cat("\n==Correlation between groups and ACIs\n")
 print(correlation)
+
+correlation.norm <- cor(Final.norm[,"Group"], Final.norm[,"ACI"],method = "spearman")
+cat("\n==Normalized Correlation between groups and ACIs\n")
+print(correlation.norm)
+
 # plot(Final[,"Group"],Final[,"ACI"])
+# plot(Final.norm[,"Group"],Final.norm[,"ACI"])
 
 sink()
 print("Finished")
