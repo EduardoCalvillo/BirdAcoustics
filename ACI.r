ead0f8413c37f60c7b3b3ff5be154550cdc5fcfe
Edgar Tamez <e.tamez25@hotmail.com>
2019-11-26 19:48:43 -0600

    BI Groups 2

--- ACI.r
+++ ACI.r
@@ -20,22 +20,68 @@ myspec <- function(wave) {
     return(spec(wave, f = as.numeric(wave@samp.rate), plot = FALSE)[,"y"])
 }
 
-
 myBI <- function(wave) {
     invisible(capture.output(output <- bioacoustic_index(wave)))
     return(output$left_area)
+}
+
+SplitBIs <- function(wave){
+    cat("Recibo"+""+ wave)
+}
+
+Idmax <- function(BIS){
+    Mayor <- 0
+    for(x in 1:length(BIS)){
+        if(BIS[[x]] > Mayor ){
+            Mayor <- BIS[[x]]
+        }
+    } 
+    return(Mayor)
+}
+
+Idmin <- function(BIS, IBmax){
+    Menor <- IBmax
+    for(x in 1:length(BIS)){
+        
+        if(BIS[[x]] < Menor ){
+            Menor <- BIS[[x]]
+            print(BIS[[x]])
+        }
+    } 
+    return(Menor)
+}
+
+IdGroup <- function(BIS, IBmax, IBmin){ 
+    Silence <- IBmin
+    Low <- Silence + ((IBmax-IBmin)/4)
+    Means <- Low + ((IBmax-IBmin)/4)
+    Tall <- IBmax
+    output <- c()
+
+   for(x in 1:length(BIS)){
+        if(BIS[[x]] <= Silence){ id <- 1}
+        if(BIS[[x]] > Silence && BIS[[x]] <= Low ){ id <- 2}
+        if(BIS[[x]] > Low && BIS[[x]] <= Means ){ id <- 3}
+        if(BIS[[x]] > Means){ id <- 4}
+    
+       output<- c(output,BIS[[x]],as.integer(id))
+   }
+
+    return(output)
+    
 }
 
 #Leer Directorio
 ##p <- "C:/Users/Eduardo Calvillo Uni/Documents/FIME/Topicos Selectos 2/AUDIOS PIA/Partes/"
 p <- "C:/Users/etame/OneDrive/Escritorio/Escuela/Escuela/Acustica/PIA/Session_5/"
 list <- unlist(list.files(path = p, pattern = ".*\\.wav", full.names = TRUE))
-shortlist <- list[1:5]
+shortlist <- list[1:8]
 
 #Tratamiento
 waves <- lapply(shortlist, readWave)
 waves <- unlist(lapply(waves, mono))
 waves <- lapply(waves, myFFilter, from = 8000, to = 9200, bandpass = FALSE )
+
 
 # Verificar que el filtro de frecuencia se haya aplicado
 # spec(waves[[2]], f = as.numeric(waves[[2]]@samp.rate), plot = TRUE)
@@ -44,10 +90,19 @@ waves <- lapply(waves, myFFilter, from = 8000, to = 9200, bandpass = FALSE )
 BIs <- lapply(waves, myBI)
 BIs
 
+##Id-Indices Min-Max
+IBmax <- Idmax(BIs)
+IBmin <- Idmin(BIs, IBmax)
+
+#Identificamos el grupo al que pertencere
+#Cada uno de los waves
+Groups <- IdGroup(BIs,IBmax,IBmin)
+Groups
+
 # Tratamiento de Amplitudes (Transformación logarítimca)
-# amplitudes <- lapply(monoWaves, env, envt = "abs", plot = FALSE)
+##amplitudes <- lapply(monoWaves, env, envt = "abs", plot = FALSE)
 logAmplitudes <- lapply(amplitudes, log1p)
-# log1p(amplitudes[[1]])
+##log1p(amplitudes[[1]])
 
 # Tratamiento de Frecuencias (Transformación logarítimca)
 # specs <- lapply(newlist, myspec)
@@ -56,3 +111,7 @@ logFrequencies <- lapply(specs, log1p)
 #Calcular ACI
 ACIs <- lapply(waves, NewACI)
 ACIs
+
+
+
+
